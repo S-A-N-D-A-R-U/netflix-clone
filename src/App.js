6a1a27d53dbca257fbace3ba600d3ef68dde3861
Naveen S Bandara <116500705+S-A-N-D-A-R-U@users.noreply.github.com>
2024-09-22 21:52:49 +0530

    create redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './components/Header';
 import Body from './components/Body';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
 
 const App = () => {
   return (
     <div>
-     <Body/>
+      <Provider store={appStore}><Body/></Provider>
     </div>
   )
 }
