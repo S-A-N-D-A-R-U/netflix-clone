56fa3336dde1ad4ddc5d2bafa01b4a3157037f6e
Naveen S Bandara <116500705+S-A-N-D-A-R-U@users.noreply.github.com>
2024-09-23 13:20:46 +0530

    Unsubscribe to the onAuthStateChanged callback

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
  };
 
  useEffect(() => {
-  onAuthStateChanged(auth, (user) => {
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
     if (user) {
       const { uid, email, displayName, photoURL } = user;
       dispatch(
@@ -39,6 +39,8 @@ const Header = () => {
       navigate("/")
     }
   });
+
+  return () => unsubscribe();
 }, []);
 
   return (
