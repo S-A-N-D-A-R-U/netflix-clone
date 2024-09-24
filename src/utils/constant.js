45b2abd466efc22126a786d43085281f4b66ec8b
Naveen S Bandara <116500705+S-A-N-D-A-R-U@users.noreply.github.com>
2024-09-24 09:10:51 +0530

    set TBMS api, moviesSlice

---
+++ src/utils/constant.js
@@ -0,0 +1,11 @@
+
+
+
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzN2Y3NTU2MjI0OWZkNjg3NzA4ODlmZGI1ODM1NmNhNiIsIm5iZiI6MTcyNzA4MDYxMC42MDA0NzcsInN1YiI6IjY2ZjEyNGQwNzMwMGE1YmEyMTNiN2ZmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AafpcHR_AopPi4J1VwJAE8mIm0-o_qJGM73Zwfnx7Cs'
+    }
+  };
